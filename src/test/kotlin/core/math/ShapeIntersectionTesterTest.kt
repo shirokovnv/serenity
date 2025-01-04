a6c1d3097236e516e9764d68df08f12ed7627e24
shirokovnv <shirokovnv@gmail.com>
2025-01-05 02:10:04 +0500

    Add basic intersection tests

---
+++ src/test/kotlin/core/math/ShapeIntersectionTesterTest.kt
@@ -0,0 +1,63 @@
+package core.math
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.*
+
+class ShapeIntersectionTesterTest {
+
+    @Test
+    fun `test rect2d intersects`() {
+        val rectA = Rect2d(Vector2(0f, 0f), Vector2(2f, 2f))
+        val rectB = Rect2d(Vector2(1f, 1f), Vector2(3f, 3f))
+        assertTrue(ShapeIntersectionTester.intersects(rectA, rectB))
+    }
+
+    @Test
+    fun `test rect2d not intersects`() {
+        val rectA = Rect2d(Vector2(0f, 0f), Vector2(1f, 1f))
+        val rectB = Rect2d(Vector2(2f, 2f), Vector2(3f, 3f))
+        assertFalse(ShapeIntersectionTester.intersects(rectA, rectB))
+    }
+
+    @Test
+    fun `test rect3d intersects`() {
+        val rectA = Rect3d(Vector3(0f, 0f, 0f), Vector3(2f, 2f, 2f))
+        val rectB = Rect3d(Vector3(1f, 1f, 1f), Vector3(3f, 3f, 3f))
+        assertTrue(ShapeIntersectionTester.intersects(rectA, rectB))
+    }
+
+    @Test
+    fun `test rect3d not intersects`() {
+        val rectA = Rect3d(Vector3(0f, 0f, 0f), Vector3(1f, 1f, 1f))
+        val rectB = Rect3d(Vector3(2f, 2f, 2f), Vector3(3f, 3f, 3f))
+        assertFalse(ShapeIntersectionTester.intersects(rectA, rectB))
+    }
+
+    @Test
+    fun `test sphere intersects`() {
+        val sphereA = Sphere(Vector3(0f, 0f, 0f), 1f)
+        val sphereB = Sphere(Vector3(1f, 0f, 0f), 1f)
+        assertTrue(ShapeIntersectionTester.intersects(sphereA, sphereB))
+    }
+
+    @Test
+    fun `test sphere not intersects`() {
+        val sphereA = Sphere(Vector3(0f, 0f, 0f), 1f)
+        val sphereB = Sphere(Vector3(3f, 0f, 0f), 1f)
+        assertFalse(ShapeIntersectionTester.intersects(sphereA, sphereB))
+    }
+
+    @Test
+    fun `test rect3d sphere intersects`() {
+        val rect = Rect3d(Vector3(0f, 0f, 0f), Vector3(2f, 2f, 2f))
+        val sphere = Sphere(Vector3(1f, 1f, 1f), 1f)
+        assertTrue(ShapeIntersectionTester.intersects(rect, sphere))
+    }
+
+    @Test
+    fun `test rect3d sphere not intersects`() {
+        val rect = Rect3d(Vector3(0f, 0f, 0f), Vector3(1f, 1f, 1f))
+        val sphere = Sphere(Vector3(3f, 3f, 3f), 1f)
+        assertFalse(ShapeIntersectionTester.intersects(rect, sphere))
+    }
+}

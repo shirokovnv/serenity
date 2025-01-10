461e0575f3d191ba4d157b6091c4e4f78725dcdd
shirokovnv <shirokovnv@gmail.com>
2025-01-10 18:41:51 +0500

    Add image data class and refactor image loader

--- src/main/kotlin/graphics/assets/texture/Texture2d.kt
+++ src/main/kotlin/graphics/assets/texture/Texture2d.kt
@@ -1,47 +1,37 @@
-package graphics.assets
+package graphics.assets.texture
 
+import graphics.assets.Asset
 import org.lwjgl.opengl.GL11
 import org.lwjgl.opengl.GL30
-import platform.services.filesystem.ImageLoader
 
-class Texture2d(fileName: String, loader: ImageLoader) : Asset {
-    private var id: Int = 0
-    private var width: Int = 0
-    private var height: Int = 0
-
-    init {
-        val rawTextureData = loader.loadImage(fileName)
-        this.id = rawTextureData[0]
-        this.width = rawTextureData[1]
-        this.height = rawTextureData[2]
-    }
+class Texture2d(private var textureData: ImageData) : Asset {
 
     override fun getId(): Int {
-        return id
+        return textureData.id
     }
 
     override fun create() {
-        id = GL11.glGenTextures()
+        textureData = ImageData(GL11.glGenTextures(), 0, 0)
     }
 
     override fun destroy() {
-        if (id != 0) {
-            GL11.glDeleteTextures(id)
+        if (textureData.id != 0) {
+            GL11.glDeleteTextures(textureData.id)
         }
-        id = 0
+        textureData = ImageData.empty()
     }
 
     override fun bind() {
-        GL11.glBindTexture(GL11.GL_TEXTURE_2D, id)
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, textureData.id)
     }
 
     override fun unbind() {
         GL11.glBindTexture(GL11.GL_TEXTURE_2D, 0)
     }
 
-    fun getWidth(): Int = width
+    fun getWidth(): Int = textureData.width
 
-    fun getHeight(): Int = height
+    fun getHeight(): Int = textureData.height
 
     fun noFilter() {
         GL11.glTexParameteri(GL11.GL_TEXTURE_2D, GL11.GL_TEXTURE_MAG_FILTER, GL11.GL_NEAREST)

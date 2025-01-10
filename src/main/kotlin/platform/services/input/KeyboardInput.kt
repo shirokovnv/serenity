64ccb104e29584a1da483e0821e51fcc2d9b7d79
shirokovnv <shirokovnv@gmail.com>
2025-01-11 03:10:19 +0500

    Setup key holding state watching

--- src/main/kotlin/platform/services/input/KeyboardInput.kt
+++ src/main/kotlin/platform/services/input/KeyboardInput.kt
@@ -5,6 +5,7 @@ import org.lwjgl.glfw.GLFW
 class KeyboardInput(private val window: Long) {
 
     private val listeners = mutableListOf<KeyboardInputListener>()
+    private val keys = mutableMapOf<Int, Boolean>()
 
     fun addListener(listener: KeyboardInputListener) {
         listeners.add(listener)
@@ -17,10 +18,12 @@ class KeyboardInput(private val window: Long) {
     fun keyCallback(window: Long, key: Int, scancode: Int, action: Int, mods: Int) {
         when (action) {
             GLFW.GLFW_PRESS -> {
+                keys[key] = true
                 listeners.forEach{it.onKeyPressed(key)}
             }
 
             GLFW.GLFW_RELEASE ->{
+                keys[key] = false
                 listeners.forEach{it.onKeyReleased(key)}
             }
         }
@@ -29,4 +32,6 @@ class KeyboardInput(private val window: Long) {
     fun isKeyPressed(key: Int): Boolean {
         return GLFW.glfwGetKey(window, key) == GLFW.GLFW_PRESS
     }
+
+    fun isKeyHolding(key: Int): Boolean = keys[key] ?: false
 }

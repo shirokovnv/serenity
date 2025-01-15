7b24f184eb1b67de6307c3ad65cd42dc2d6014be
shirokovnv <shirokovnv@gmail.com>
2025-01-15 21:38:10 +0500

    Improve sun movement system

--- src/main/kotlin/modules/light/SunLightController.kt
+++ src/main/kotlin/modules/light/SunLightController.kt
@@ -6,8 +6,14 @@ import org.lwjgl.glfw.GLFW
 import platform.services.input.KeyboardInput
 import platform.services.input.KeyboardInputListener
 
-class SunLightController(private val moveSpeed: Float = 0.1f): Behaviour(), KeyboardInputListener {
+enum class SunMovement {
+    FORWARD,
+    BACKWARD
+}
+
+class SunLightController(private val moveSpeed: Float = 0.01f): Behaviour(), KeyboardInputListener {
     private lateinit var sunLightManager: SunLightManager
+    private val movement = mutableMapOf<SunMovement, Boolean>()
 
     override fun create() {
         sunLightManager = SunLightManager()
@@ -17,6 +23,16 @@ class SunLightController(private val moveSpeed: Float = 0.1f): Behaviour(), Keyb
     }
 
     override fun update(deltaTime: Float) {
+        val keyboardInput = Object.services.getService<KeyboardInput>()!!
+        movement[SunMovement.FORWARD] = keyboardInput.isKeyHolding(GLFW.GLFW_KEY_EQUAL)
+        movement[SunMovement.BACKWARD] = keyboardInput.isKeyHolding(GLFW.GLFW_KEY_MINUS)
+
+        movement.forEach { (direction, isPressed) ->
+            if(isPressed){
+                processMovement(direction)
+            }
+        }
+
         sunLightManager.update()
     }
 
@@ -24,12 +40,29 @@ class SunLightController(private val moveSpeed: Float = 0.1f): Behaviour(), Keyb
     }
 
     override fun onKeyPressed(key: Int) {
-        when(key) {
-            GLFW.GLFW_KEY_EQUAL -> sunLightManager.setTimeOfDay(sunLightManager.timeOfDay() + moveSpeed)
-            GLFW.GLFW_KEY_MINUS -> sunLightManager.setTimeOfDay(sunLightManager.timeOfDay() - moveSpeed)
+        getDirection(key)?.let { direction ->
+            movement[direction] = true
         }
     }
 
     override fun onKeyReleased(key: Int) {
+        getDirection(key)?.let { direction ->
+            movement[direction] = false
+        }
+    }
+
+    private fun getDirection(key: Int): SunMovement? {
+        return when (key) {
+            GLFW.GLFW_KEY_EQUAL -> SunMovement.FORWARD
+            GLFW.GLFW_KEY_MINUS -> SunMovement.BACKWARD
+            else -> null
+        }
+    }
+
+    private fun processMovement(direction: SunMovement) {
+        when(direction) {
+            SunMovement.FORWARD -> sunLightManager.setTimeOfDay(sunLightManager.timeOfDay() + moveSpeed)
+            SunMovement.BACKWARD -> sunLightManager.setTimeOfDay(sunLightManager.timeOfDay() - moveSpeed)
+        }
     }
 }

5da1a4a4e87e6f659683dc382c59df08b71e903a
shirokovnv <shirokovnv@gmail.com>
2026-08-29 16:18:51 +0500

    Add heightmap generator to quadtree scene

--- src/main/kotlin/modules/terrain/quadtree/QuadTreeTerrain.kt
+++ src/main/kotlin/modules/terrain/quadtree/QuadTreeTerrain.kt
@@ -8,9 +8,9 @@ import modules.terrain.TerrainNormalRenderer
 class QuadTreeTerrain(terrConfig: QuadTreeTerrainConfig, lodConfig: QuadTreeLoDConfig): Object() {
     init {
         val grass = ElevationData(0f, 1f, -1f, 1f, 1f)
-        val dirt = ElevationData(0.0f, 0.2f, 0.75f, 1.0f, 5f)
-        val rock = ElevationData(0.0f, 0.5f, 0f, 0.85f, 10f)
-        val snow = ElevationData(0.5f, 0.8f, 0.75f, 1f, 20f)
+        val dirt = ElevationData(0.0f, 0.5f, 0.75f, 1.0f, 5f)
+        val rock = ElevationData(0.4f, 0.8f, 0f, 0.55f, 10f)
+        val snow = ElevationData(0.6f, 1.0f, 0.75f, 1f, 20f)
 
         val elevationData = arrayOf(
             grass,
--- src/main/kotlin/modules/terrain/quadtree/QuadTreeTerrainScene.kt
+++ src/main/kotlin/modules/terrain/quadtree/QuadTreeTerrainScene.kt
@@ -1,24 +1,44 @@
 package modules.terrain.quadtree
 
 import core.management.Resources
-import graphics.assets.texture.TextureFactory
+import core.math.noise.OctaveNoiseParams
+import core.math.noise.PerlinNoise
 import modules.terrain.BaseTerrainScene
 import modules.terrain.TerrainSceneParams
 import modules.terrain.heightmap.Heightmap
+import modules.terrain.heightmap.filters.DomainWarpFilter
+import modules.terrain.heightmap.filters.ErosionFilter
+import modules.terrain.heightmap.filters.MountainMaskFilter
+import modules.terrain.heightmap.filters.RidgedFilter
+import modules.terrain.heightmap.generators.multi_fractal.MultiFractalGenerator
+import modules.terrain.heightmap.generators.multi_fractal.MultiFractalParams
 
 class QuadTreeTerrainScene(params: TerrainSceneParams): BaseTerrainScene(params) {
     override fun initializeTerrain() {
-        val heightmap = Heightmap(
-            TextureFactory.fromPerlinNoise(
-                1024,
-                1024,
-                0.007f,
-                5,
-                1.0f,
-                0.46f
+        val noise = PerlinNoise.defaultNoiseInstance
+        val noiseParams = OctaveNoiseParams(
+            0.005f,
+            8,
+            1.0f,
+            0.35f,
+        )
+
+        val heightmap = Heightmap.fromGenerator(
+            MultiFractalGenerator(),
+            MultiFractalParams(
+                noise,
+                noiseParams,
+                listOf(
+                    DomainWarpFilter(noise, noiseParams),
+                    RidgedFilter(),
+                    MountainMaskFilter(),
+                    ErosionFilter()
+                )
             ),
+            1024,
+            1024,
+            params.worldOffset,
             params.worldScale,
-            params.worldOffset
         )
         Resources.put<Heightmap>(heightmap)
 
